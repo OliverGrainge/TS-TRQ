5d9fdeecbe20f22ea2f189e6451bd588e2674b10
Oliver Grainge <oliver@grainge.me>
2025-09-02 09:25:57 +1000

    updating layers

--- quant/layers/__init__.py
+++ quant/layers/__init__.py
@@ -1,14 +1,8 @@
-from .nbitlinear import NBitLinear
-from .trqlinear import TRQLinear
 from .tsvdlinear import TSVDLinear
-from .tsvdtlinear import TSVDTLinear
 
 __all__ = ["TRQLinear", "NBitLinear", "TSVDLinear"]
 
 
 QUANT_LAYERS = {
-    "trqlinear": TRQLinear,
-    "nbitlinear": NBitLinear,
     "tsvdlinear": TSVDLinear,
-    "tsvdtlinear": TSVDTLinear,
 }
