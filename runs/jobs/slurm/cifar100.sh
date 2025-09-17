1092cbd5cee7f87ffe58e72f5b352cc1e31ac9e4
oliver grainge <oliver@grainge.me>
2025-09-17 05:15:44 +0100

    adding code

--- runs/jobs/slurm/cifar100.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-python train.py runs/configs/resnet/cifar100/resnet18_untrained_cifar100_linear.yaml
-python train.py runs/configs/resnet/cifar100/resnet18_cifar100_tlinear.yaml
-python train.py runs/configs/resnet/cifar100/resnet18_cifar100_tsvdlinear.yaml
-python train.py runs/configs/resnet/cifar100/resnet18_cifar100_linear.yaml
